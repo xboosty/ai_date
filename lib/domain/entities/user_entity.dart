df17cfcea618a8ea0e36dfca9fcb664296285392
ariel.rodriguez <ariel.rodriguez@ntsprint.com>
2023-10-18 15:43:28 -0700

    Add changes

--- lib/domain/entities/user_entity.dart
+++ lib/domain/entities/user_entity.dart
@@ -1,7 +1,7 @@
 class UserEntity {
   final int? id;
   final DateTime birthDate;
-  final String fullName;
+  final String name;
   final String identity;
   final int genderId;
   final String gender;
@@ -17,7 +17,7 @@ class UserEntity {
   UserEntity({
     this.id,
     required this.birthDate,
-    required this.fullName,
+    required this.name,
     required this.identity,
     required this.genderId,
     required this.gender,
@@ -33,35 +33,35 @@ class UserEntity {
 
   get isVerify => null;
 
-  // factory UserEntity.fromJson(Map<String, dynamic> json) => UserEntity(
-  //     id: json["id"],
-  //     birthDate: DateTime.parse(json["birthDate"]),
-  //     fullName: json["fullName"],
-  //     identity: json["identity"],
-  //     genderId: json["genderId"],
-  //     gender: json["gender"],
-  //     email: json["email"],
-  //     phone: json["phone"],
-  //     statusId: json["statusId"],
-  //     status: json["status"],
-  //     avatar: json["avatar"],
-  //     avatarMimeType: json["avatarMimeType"],
-  //     sexualOrientation: json["sexualOrientation"],
-  // );
+  factory UserEntity.fromJson(Map<String, dynamic> json) => UserEntity(
+        id: json["id"],
+        birthDate: DateTime.parse(json["birthDate"]),
+        name: json["name"],
+        identity: json["identity"],
+        genderId: json["genderId"],
+        gender: json["gender"],
+        email: json["email"],
+        phone: json["phone"],
+        statusId: json["statusId"],
+        status: json["status"],
+        avatar: json["avatar"],
+        avatarMimeType: json["avatarMimeType"],
+        sexualOrientation: json["sexualOrientation"],
+      );
 
-  // Map<String, dynamic> toJson() => {
-  //     "id": id,
-  //     "birthDate": birthDate.toIso8601String(),
-  //     "fullName": fullName,
-  //     "identity": identity,
-  //     "genderId": genderId,
-  //     "gender": gender,
-  //     "email": email,
-  //     "phone": phone,
-  //     "statusId": statusId,
-  //     "status": status,
-  //     "avatar": avatar,
-  //     "avatarMimeType": avatarMimeType,
-  //     "sexualOrientation": sexualOrientation,
-  // };
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "birthDate": birthDate.toIso8601String(),
+        "name": name,
+        "identity": identity,
+        "genderId": genderId,
+        "gender": gender,
+        "email": email,
+        "phone": phone,
+        "statusId": statusId,
+        "status": status,
+        "avatar": avatar,
+        "avatarMimeType": avatarMimeType,
+        "sexualOrientation": sexualOrientation,
+      };
 }
