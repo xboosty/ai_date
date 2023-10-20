792a9b5156ed962667871f30f0cd2a0f9b9e68dd
ariel.rodriguez <ariel.rodriguez@ntsprint.com>
2023-10-20 14:46:24 -0700

    Add changes logo splash

--- lib/presentation/screens/interview/interview_chat_screen.dart
+++ lib/presentation/screens/interview/interview_chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:animate_do/animate_do.dart';
 
 import '../../../config/config.dart' show AppTheme, Strings;
 
@@ -63,36 +64,40 @@ class InterviewChatScreen extends StatelessWidget {
               child: SingleChildScrollView(
                 child: Column(
                   children: [
-                    const Padding(
-                      padding:
-                          EdgeInsets.symmetric(vertical: 10, horizontal: 10.0),
-                      child: Text(
-                        'Great, let\'s start with some questions about you and what makes you unique. These questions will cover your background, values, and what shapes your world. Please take your time and answer as honestly as possible.',
-                        style: TextStyle(
-                          color: Color(0xFF6C2EBC),
-                          fontSize: 20,
-                          fontFamily: Strings.fontFamily,
-                          fontWeight: FontWeight.w600,
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 10, horizontal: 10.0),
+                      child: FadeIn(
+                        child: const Text(
+                          'Great, let\'s start with some questions about you and what makes you unique. These questions will cover your background, values, and what shapes your world. Please take your time and answer as honestly as possible.',
+                          style: TextStyle(
+                            color: Color(0xFF6C2EBC),
+                            fontSize: 20,
+                            fontFamily: Strings.fontFamily,
+                            fontWeight: FontWeight.w600,
+                          ),
                         ),
                       ),
                     ),
                     SizedBox(height: size.height * 0.01),
-                    Chip(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(50),
+                    FadeIn(
+                      child: Chip(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50),
+                          side: BorderSide.none,
+                        ),
                         side: BorderSide.none,
-                      ),
-                      side: BorderSide.none,
-                      backgroundColor: const Color(0xFFCCC1EA),
-                      label: const Padding(
-                        padding: EdgeInsets.all(5.0),
-                        child: Text(
-                          'Today',
-                          style: TextStyle(
-                            color: Color(0xFF261638),
-                            fontSize: 12,
-                            fontFamily: Strings.fontFamily,
-                            fontWeight: FontWeight.w600,
+                        backgroundColor: const Color(0xFFCCC1EA),
+                        label: const Padding(
+                          padding: EdgeInsets.all(5.0),
+                          child: Text(
+                            'Today',
+                            style: TextStyle(
+                              color: Color(0xFF261638),
+                              fontSize: 12,
+                              fontFamily: Strings.fontFamily,
+                              fontWeight: FontWeight.w600,
+                            ),
                           ),
                         ),
                       ),
@@ -120,9 +125,8 @@ class InterviewChatScreen extends StatelessWidget {
                       style: TextStyle(
                         color: Color(0xFFBDC0D6),
                         fontSize: 12,
-                        fontFamily: 'Montserrat',
+                        fontFamily: Strings.fontFamily,
                         fontWeight: FontWeight.w500,
-                        height: 0.12,
                       ),
                     ),
                     IconButton(onPressed: () {}, icon: const Icon(Icons.mic))
@@ -146,61 +150,63 @@ class _ChatUser extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Container(
-                width: size.width * 0.89,
-                decoration: BoxDecoration(
-                  color: Colors.purple.shade300,
-                  borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(20),
-                    topRight: Radius.circular(20),
-                    bottomLeft: Radius.circular(20),
-                    bottomRight: Radius.circular(5),
+    return FadeIn(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  width: size.width * 0.89,
+                  decoration: BoxDecoration(
+                    color: Colors.purple.shade300,
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20),
+                      bottomLeft: Radius.circular(20),
+                      bottomRight: Radius.circular(5),
+                    ),
                   ),
-                ),
-                child: const Column(
-                  children: [
-                    ListTile(
-                      title: Text(
-                        'Could you share a story from your life that you think shaped who you are today?',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 14,
-                          fontFamily: Strings.fontFamily,
-                          fontWeight: FontWeight.w500,
+                  child: const Column(
+                    children: [
+                      ListTile(
+                        title: Text(
+                          'Could you share a story from your life that you think shaped who you are today?',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 14,
+                            fontFamily: Strings.fontFamily,
+                            fontWeight: FontWeight.w500,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
-        const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 3.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Text(
-                '01:45',
-                style: TextStyle(
-                  color: Color(0xFFBDC0D6),
-                  fontSize: 12,
-                  fontFamily: Strings.fontFamily,
-                  fontWeight: FontWeight.w600,
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 3.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  '01:45',
+                  style: TextStyle(
+                    color: Color(0xFFBDC0D6),
+                    fontSize: 12,
+                    fontFamily: Strings.fontFamily,
+                    fontWeight: FontWeight.w600,
+                  ),
                 ),
-              ),
-            ],
-          ),
-        )
-      ],
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
@@ -214,65 +220,67 @@ class _ChatAI extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Container(
-                width: size.width * 0.89,
-                decoration: const BoxDecoration(
-                  gradient: AppTheme.linearGradientTopRightBottomLeft,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(20),
-                    topRight: Radius.circular(20),
-                    bottomLeft: Radius.circular(5),
-                    bottomRight: Radius.circular(20),
+    return FadeIn(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  width: size.width * 0.89,
+                  decoration: const BoxDecoration(
+                    gradient: AppTheme.linearGradientTopRightBottomLeft,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20),
+                      bottomLeft: Radius.circular(5),
+                      bottomRight: Radius.circular(20),
+                    ),
                   ),
-                ),
-                child: const Column(
-                  children: [
-                    ListTile(
-                      leading: CircleAvatar(
-                        backgroundColor: Colors.red,
-                        backgroundImage:
-                            AssetImage('assets/imgs/ai_avatar.png'),
-                      ),
-                      title: Text(
-                        'Could you share a story from your life that you think shaped who you are today?',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 14,
-                          fontFamily: Strings.fontFamily,
-                          fontWeight: FontWeight.w500,
+                  child: const Column(
+                    children: [
+                      ListTile(
+                        leading: CircleAvatar(
+                          backgroundColor: Colors.red,
+                          backgroundImage:
+                              AssetImage('assets/imgs/ai_avatar.png'),
+                        ),
+                        title: Text(
+                          'Could you share a story from your life that you think shaped who you are today?',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 14,
+                            fontFamily: Strings.fontFamily,
+                            fontWeight: FontWeight.w500,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
-        const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 3.0),
-          child: Row(
-            children: [
-              Text(
-                '01:45',
-                style: TextStyle(
-                  color: Color(0xFFBDC0D6),
-                  fontSize: 12,
-                  fontFamily: Strings.fontFamily,
-                  fontWeight: FontWeight.w600,
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 3.0),
+            child: Row(
+              children: [
+                Text(
+                  '01:45',
+                  style: TextStyle(
+                    color: Color(0xFFBDC0D6),
+                    fontSize: 12,
+                    fontFamily: Strings.fontFamily,
+                    fontWeight: FontWeight.w600,
+                  ),
                 ),
-              ),
-            ],
-          ),
-        )
-      ],
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
