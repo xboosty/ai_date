f3825dcb903da292730fbf3abd9faeb065c672d6
ariel.rodriguez <ariel.rodriguez@ntsprint.com>
2023-11-02 20:09:59 -0700

    change photo profiles and fixed error image

--- lib/presentation/screens/home/discover/suggestion_page.dart
+++ lib/presentation/screens/home/discover/suggestion_page.dart
@@ -251,90 +251,10 @@ class _CardCouple extends StatelessWidget {
           alignment: Alignment.bottomCenter,
           clipBehavior: Clip.none,
           children: [
-            // Container(
-            //   padding: const EdgeInsets.all(1.5),
-            //   decoration: BoxDecoration(
-            //     borderRadius: BorderRadius.circular(20),
-            //     gradient: AppTheme.linearGradient,
-            //   ),
-            //   child: Container(
-            //     width: size.width * 0.78,
-            //     height: size.height * 0.50,
-            //     padding: const EdgeInsets.all(15),
-            //     decoration: BoxDecoration(
-            //       borderRadius: BorderRadius.circular(20),
-            //       color: const Color(0xFFEFF0FB),
-            //       image: DecorationImage(
-            //         image: imageProvider,
-            //         fit: BoxFit.cover,
-            //       ),
-            //     ),
-            //     child:
-            //   ),
-            // ),
-
             CardProfile(
               user: user,
               imageProvider: imageProvider,
             ),
-            //       Column(
-            //   mainAxisAlignment: MainAxisAlignment.start,
-            //   crossAxisAlignment: CrossAxisAlignment.center,
-            //   mainAxisSize: MainAxisSize.max,
-            //   children: [
-            //     Row(
-            //       children: [
-            //         IconButton(
-            //           onPressed: () {},
-            //           style: IconButton.styleFrom(
-            //             backgroundColor: Colors.white,
-            //           ),
-            //           icon: const Icon(
-            //             Icons.note_add_rounded,
-            //             color: Color(0xFFD9D9D9),
-            //           ),
-            //         )
-            //       ],
-            //     ),
-            //     const Spacer(),
-            //     Text(
-            //       '${user.name} (24)',
-            //       style: const TextStyle(
-            //         color: Color(0xFF9CA4BF),
-            //         fontSize: 22,
-            //         fontFamily: Strings.fontFamily,
-            //         fontWeight: FontWeight.w600,
-            //       ),
-            //     ),
-            //     SizedBox(height: size.height * 0.02),
-            //     const Text(
-            //       'Lives in New York',
-            //       style: TextStyle(
-            //         color: Color(0xFF9CA4BF),
-            //         fontSize: 12,
-            //         fontFamily: Strings.fontFamily,
-            //         fontWeight: FontWeight.w600,
-            //       ),
-            //     ),
-            //     SizedBox(height: size.height * 0.02),
-            //     FilledButton(
-            //       onPressed: () {},
-            //       style: FilledButton.styleFrom(
-            //         backgroundColor: Colors.white,
-            //       ),
-            //       child: const Text(
-            //         '95% match',
-            //         style: TextStyle(
-            //           color: Color(0xFF9CA4BF),
-            //           fontSize: 12,
-            //           fontFamily: Strings.fontFamily,
-            //           fontWeight: FontWeight.w600,
-            //         ),
-            //       ),
-            //     )
-            //   ],
-            // ),
-
             Positioned(
               bottom: -25.0,
               child: Row(
@@ -367,125 +287,15 @@ class _CardCouple extends StatelessWidget {
           ],
         ),
       ),
-      placeholder: (context, url) =>
-          const Center(child: CircularProgressIndicator()),
-      errorWidget: (context, url, error) => Container(
-        width: size.width * 0.80,
-        height: size.height,
-        alignment: Alignment.center,
-        // color: Colors.blue,
-        child: Stack(
-          alignment: Alignment.bottomCenter,
-          clipBehavior: Clip.none,
-          children: [
-            Container(
-              padding: const EdgeInsets.all(1.5),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                gradient: AppTheme.linearGradient,
-              ),
-              child: Container(
-                width: size.width * 0.78,
-                height: size.height * 0.50,
-                padding: const EdgeInsets.all(15),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: const Color(0xFFEFF0FB),
-                  image: const DecorationImage(
-                    image: AssetImage('assets/imgs/photo_camera_front.png'),
-                    fit: BoxFit.contain,
-                  ),
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    Row(
-                      children: [
-                        IconButton(
-                          onPressed: () {},
-                          style: IconButton.styleFrom(
-                            backgroundColor: Colors.white,
-                          ),
-                          icon: const Icon(
-                            Icons.note_add_rounded,
-                            color: Color(0xFFD9D9D9),
-                          ),
-                        )
-                      ],
-                    ),
-                    const Spacer(),
-                    Text(
-                      '${user.name} (24)',
-                      style: const TextStyle(
-                        color: Color(0xFF9CA4BF),
-                        fontSize: 22,
-                        fontFamily: Strings.fontFamily,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                    SizedBox(height: size.height * 0.02),
-                    const Text(
-                      'Lives in New York',
-                      style: TextStyle(
-                        color: Color(0xFF9CA4BF),
-                        fontSize: 12,
-                        fontFamily: Strings.fontFamily,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                    SizedBox(height: size.height * 0.02),
-                    FilledButton(
-                      onPressed: () {},
-                      style: FilledButton.styleFrom(
-                        backgroundColor: Colors.white,
-                      ),
-                      child: const Text(
-                        '95% match',
-                        style: TextStyle(
-                          color: Color(0xFF9CA4BF),
-                          fontSize: 12,
-                          fontFamily: Strings.fontFamily,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ),
-            Positioned(
-              bottom: -25.0,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  CircularOutlineGradientButton(
-                    onTap: () {
-                      controller.swipeLeft();
-                    },
-                    width: 56.0,
-                    height: 56.0,
-                    child: const Icon(
-                      Icons.close,
-                      color: Color.fromARGB(255, 209, 70, 15),
-                      size: 32,
-                    ),
-                  ),
-                  SizedBox(width: size.width * 0.30),
-                  CircularGradientButton(
-                    heroTag: 'Like',
-                    callback: () {
-                      controller.swipeRight();
-                    },
-                    gradient: AppTheme.linearGradientReverse,
-                    child: const Icon(Icons.favorite, size: 32),
-                  ),
-                ],
-              ),
-            )
-          ],
-        ),
+      placeholder: (context, url) => CouplePlaceHolderCard(
+        user: user,
+        controller: controller,
+        assetImage: 'assets/imgs/loading.gif',
+      ),
+      errorWidget: (context, url, error) => CouplePlaceHolderCard(
+        user: user,
+        controller: controller,
+        assetImage: 'assets/imgs/no-image.jpg',
       ),
     );
   }
