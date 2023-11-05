8185109c263a26e21d3ec3222b074f7c52a02271
ariel.rodriguez <ariel.rodriguez@ntsprint.com>
2023-11-05 11:48:17 -0800

    fixed error edit profile upload photo

--- lib/config/helpers/convert_file_multipartfile.dart
+++ lib/config/helpers/convert_file_multipartfile.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 import 'package:dio/dio.dart';
+import 'package:http_parser/http_parser.dart';
+import 'package:mime/mime.dart';
 
 Future<List<MultipartFile>> convertFileListToMultipartFileList(
     List<File?> fileList) async {
@@ -7,9 +9,13 @@ Future<List<MultipartFile>> convertFileListToMultipartFileList(
 
   for (var file in fileList) {
     if (file != null) {
+      var mimeTypeData =
+          lookupMimeType(file.path, headerBytes: [0xFF, 0xD8])?.split('/');
       String fileName = file.path.split('/').last;
-      MultipartFile multipartFile =
-          await MultipartFile.fromFile(file.path, filename: fileName);
+      MultipartFile multipartFile = await MultipartFile.fromFile(file.path,
+          filename: fileName,
+          contentType:
+              MediaType(mimeTypeData?[0] ?? '', mimeTypeData?[1] ?? ''));
       multipartFiles.add(multipartFile);
     }
   }
