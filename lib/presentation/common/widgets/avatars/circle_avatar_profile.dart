b5e06d318eba9bd855aac7d37617f0332c83c061
ariel.rodriguez <ariel.rodriguez@ntsprint.com>
2023-11-08 13:49:08 -0800

    Fixed circular avatar

--- lib/presentation/common/widgets/avatars/circle_avatar_profile.dart
+++ lib/presentation/common/widgets/avatars/circle_avatar_profile.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../config/config.dart' show AppTheme;
@@ -9,20 +10,59 @@ class CircleAvatarProfile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 60,
-      height: 60,
-      decoration: const BoxDecoration(
-        shape: BoxShape.circle,
-        gradient: AppTheme.linearGradient,
-      ),
-      child: Container(
-        margin: const EdgeInsets.all(2),
-        decoration: BoxDecoration(
+    return CachedNetworkImage(
+      imageUrl: image,
+      imageBuilder: (context, imageProvider) => Container(
+        width: 60,
+        height: 60,
+        decoration: const BoxDecoration(
           shape: BoxShape.circle,
-          image: DecorationImage(
-            image: AssetImage(image),
-            fit: BoxFit.cover,
+          gradient: AppTheme.linearGradient,
+        ),
+        child: Container(
+          margin: const EdgeInsets.all(2),
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            image: DecorationImage(
+              image: imageProvider,
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+      ),
+      placeholder: (context, url) => Container(
+        width: 60,
+        height: 60,
+        decoration: const BoxDecoration(
+          shape: BoxShape.circle,
+          gradient: AppTheme.linearGradient,
+        ),
+        child: Container(
+          margin: const EdgeInsets.all(2),
+          decoration: const BoxDecoration(
+            shape: BoxShape.circle,
+            image: DecorationImage(
+              image: AssetImage('assets/imgs/loading.gif'),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+      ),
+      errorWidget: (context, url, error) => Container(
+        width: 60,
+        height: 60,
+        decoration: const BoxDecoration(
+          shape: BoxShape.circle,
+          gradient: AppTheme.linearGradient,
+        ),
+        child: Container(
+          margin: const EdgeInsets.all(2),
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            image: DecorationImage(
+              image: AssetImage('assets/imgs/no-image.jpg'),
+              fit: BoxFit.cover,
+            ),
           ),
         ),
       ),
